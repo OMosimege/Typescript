46b614b63ab906340774f11f2cb3b3ca364e728d
OMosimege <onalerona.mosimege@gmail.com>
2023-10-19 10:43:07 +0200

    typescript tutorial

---
+++ annotations/variables.ts
@@ -0,0 +1,11 @@
+const importantDates: (string | Date)[] = [];
+
+//tuple
+const carbonated = {
+  color: "brown",
+  carbonated: true,
+  sugar: 40
+}
+type Drink = [string, boolean, number]
+const pepsi: Drink = ["brown", true, 40]
+const tea: Drink = ["brown", false, 0]
